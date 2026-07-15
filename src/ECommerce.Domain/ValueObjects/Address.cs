802cb43c0eae47f1e73632bc90d86733fe3b9789
fuadfaruk <fuadfaruk707@gmail.com>
2026-07-15 23:54:57 +0600

    Add Address value object record to domain layer

---
+++ src/ECommerce.Domain/ValueObjects/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.ValueObjects
+{
+    public sealed record Address
+    {
+        private Address()
+        {
+        }
+
+        public Address(string street, string city, string state, string postalCode, string country)
+        {
+
+        }
+    }
+}
