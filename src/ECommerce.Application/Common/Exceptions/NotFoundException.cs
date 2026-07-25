c2247b5a876e8504f19c323def4391385099879f
fuadfaruk <fuadfaruk707@gmail.com>
2026-07-25 15:35:02 +0600

    Add validation behavior and custom exceptions to Common

---
+++ src/ECommerce.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Common.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when a requested resource is not found.
+    /// </summary>
+    public sealed class NotFoundException(string name, object key)
+        : Exception($"{name} with key '{key}' was not found.");
+
+    /// <summary>
+    /// Exception thrown when a user attempts to access a resource they do not have permission to access.
+    /// </summary>
+    public sealed class ForbiddenAcessException(string message)
+        : Exception(message);
+
+    /// <summary>
+    /// Exception thrown when a conflict occurs, such as when trying to create a resource that already exists.
+    /// </summary>
+    public sealed class ConflictException(string message)
+        : Exception(message);
+}
