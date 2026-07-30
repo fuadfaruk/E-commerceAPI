0b4a94057607b143fa32c9d6db1a0f16d4b514c0
fuadfaruk <fuadfaruk707@gmail.com>
2026-07-30 23:46:05 +0600

    Add ProductsController

---
+++ src/ECommerce.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebApi.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
